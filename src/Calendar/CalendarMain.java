6bbd12b4648631e5343e0759c7aa9ca8dfbc7954
user <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>
2021-08-19 01:17:25 +0900

    캘린더 첫날 요일 설정기능 추가

--- src/Calendar/CalendarMain.java
+++ src/Calendar/CalendarMain.java
@@ -17,23 +17,44 @@ class Calendar{
 		}
 		return MaxDays[month-1];
 	}
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("    <<%4d %3d>>\n",year, month);
-		System.out.println("SU MO TU WE TH FR SA");
-		System.out.println("--------------------");
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
 		
+		for(int i=0; i<weekday; i++) {
+			System.out.print("   ");//3칸
+		}
 		int maxDay=getMaxDaysOfMonth(year, month);
-		for(int i=1; i<=maxDay;i++) {
+		
+		int count=7-weekday;
+		for(int i=1; i<=count;i++) {
 			System.out.printf("%3d",i);
-			if(i%7==0) {
+		}
+		System.out.println();
+		for(int i=count+1; i<=maxDay;i++) {
+			if(count==7) count=0;
+			System.out.printf("%3d",i);
+			if(i%7==count) {
 				System.out.println();
 			}
 		}
-		System.out.println("\n");
+		System.out.println();
 	}
 }
 
-public class CalendarMain{	
+public class CalendarMain{
+	
+	public int parseDay(String week) {
+		if(week.equals("SU")) return 0;
+		else if(week.equals("Mo")) return 1;
+		else if(week.equals("TU")) return 2;
+		else if(week.equals("WE")) return 3;
+		else if(week.equals("TH")) return 4;
+		else if(week.equals("FR")) return 5;
+		else if(week.equals("SA")) return 6;
+		else return 0;
+	}
 
 	public void runPrompt() {
 		Scanner sc=new Scanner(System.in);
@@ -41,13 +62,15 @@ public class CalendarMain{
 		
 		int month=-1;
 		int year=-1;
+		int weekday=0;
 		while(true) {
 			System.out.print("연도를 입력하세요\nYear>>");
 			year=sc.nextInt();
 			System.out.print("달을 입력하세요\nMonth>>");
 			month=sc.nextInt();
-			System.out.print("첫 요일을 입력하세요\n>>");
-			String nameOfWeek=sc.nextLine();
+			System.out.print("첫날의 요일을 입력하세요\n>>");
+			String nameOfWeek=sc.next();
+			weekday=parseDay(nameOfWeek);
 			if(month==-1){
 				break;
 			}
@@ -55,7 +78,7 @@ public class CalendarMain{
 			if(month>12) {
 				continue;
 			}
-			cal.printCalendar(year, month);
+			cal.printCalendar(year, month, weekday);
 		}
 		System.out.println("Bye");
 		sc.close();
